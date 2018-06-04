3e1bf45b74e41c4925b04445e95ee76e3d658405
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-04 04:11:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,11 @@
 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-  return "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 
 speak_to_grandma("Hi Nana, how are you?")
